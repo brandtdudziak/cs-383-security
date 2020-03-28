f904812cecb7b4fa664715631b653ebc3f77cb75
Brandt <brandtdudziak@gmail.com>
2020-03-28 13:43:42 -0400

    least sig text done

--- project4.py
+++ project4.py
@@ -1,9 +1,10 @@
 import numpy as np
 import imageio
 import bitarray
+from bitarray.util import ba2int
 
-def text():
-    img = imageio.imread("sampleImages/hide_text.png")
+def text_with_header_least_sig(image):
+    img = imageio.imread(image)
     height, width, _ = img.shape
     print("Height:", height, "Width:", width)
 
@@ -15,22 +16,23 @@ def text():
                chars.append(str(img[r,c,0] & 1))
                chars.append(str(img[r,c,1] & 1))
                chars.append(str(img[r,c,2] & 1))
-               count += 1
-    output = "".join(chars)
-    length = output[0:32]
-    print(length + "\n")
+               count += 3
+    output = "".join(chars)[0:32]
+    length = ba2int(bitarray.bitarray(output))
+    print(length)
 
     chars = []
     count = 0
     for r in range(height):
         for c in range(width):
-            if count < 1600:
+            if count < (length * 8) + 32:
                 chars.append(str(img[r,c,0] & 1))
                 chars.append(str(img[r,c,1] & 1))
                 chars.append(str(img[r,c,2] & 1))
-                count += 1
+                count += 3
     output = bitarray.bitarray("".join(chars))
-    print(output.tobytes()[4:573])
+    print(output.tobytes()[4:length + 4])
 
 if __name__ == "__main__":
-    text()
+    text_with_header_least_sig("sampleImages/hide_text.png")
+    # 2 Least significants
