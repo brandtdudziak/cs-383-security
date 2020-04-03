8c58d8a4461da107c67a18620d5196301efc51d1
Brandt <brandtdudziak@gmail.com>
2020-04-03 11:51:48 -0400

    detect image and more bits

--- project4.py
+++ project4.py
@@ -1,9 +1,10 @@
 import numpy as np
 import imageio
-import bitarray
-from bitarray.util import ba2int
+from bitarray import bitarray
+from bitarray.util import ba2int, int2ba
+import re
 
-def get_header(image, header_start, header_length):
+def get_header(image, header_start, header_length, bits):
     img = imageio.imread(image)
     height, width, _ = img.shape
 
@@ -12,19 +13,20 @@ def get_header(image, header_start, header_length):
     for r in range(height):
         for c in range(width):
             if count < header_start + header_length:
-               chars.append(str(img[r,c,0] & 1))
-               chars.append(str(img[r,c,1] & 1))
-               chars.append(str(img[r,c,2] & 1))
-               count += 3
+                previous_length = len(chars)
+                chars.extend(list(str(int2ba((img[r,c,0] & bits).item())).split('\'')[1]))
+                chars.extend(list(str(int2ba((img[r,c,1] & bits).item())).split('\'')[1]))
+                chars.extend(list(str(int2ba((img[r,c,2] & bits).item())).split('\'')[1]))
+                count += len(chars) - previous_length
     output = "".join(chars)[header_start:header_start + header_length]
-    return ba2int(bitarray.bitarray(output))
+    return ba2int(bitarray(output))
 
-def text_with_header_least_sig(image):
+def text_with_header(image, bits):
     img = imageio.imread(image)
     height, width, _ = img.shape
     print("Height:", height, "Width:", width)
 
-    length = get_header(image, 0, 32)
+    length = get_header(image, 0, 32, bits)
     print(length)
 
     chars = []
@@ -32,11 +34,12 @@ def text_with_header_least_sig(image):
     for r in range(height):
         for c in range(width):
             if count < (length * 8) + 32:
-                chars.append(str(img[r,c,0] & 1))
-                chars.append(str(img[r,c,1] & 1))
-                chars.append(str(img[r,c,2] & 1))
-                count += 3
-    output = bitarray.bitarray("".join(chars))
+                previous_length = len(chars)
+                chars.extend(list(str(int2ba((img[r,c,0] & bits).item())).split('\'')[1]))
+                chars.extend(list(str(int2ba((img[r,c,1] & bits).item())).split('\'')[1]))
+                chars.extend(list(str(int2ba((img[r,c,2] & bits).item())).split('\'')[1]))
+                count += len(chars) - previous_length
+    output = bitarray("".join(chars))
     print(output.tobytes()[4:length + 4])
 
 def hidden_image(image):
@@ -44,8 +47,8 @@ def hidden_image(image):
     height, width, channels = img.shape
     print("Height:", height, "Width:", width, "Number of Channels:", channels)
 
-    hidden_height = get_header(image, 0, 32)
-    hidden_width = get_header(image, 32, 32)
+    hidden_height = get_header(image, 0, 32, 1)
+    hidden_width = get_header(image, 32, 32, 1)
 
     print("Hidden height:", hidden_height, "Hidden width:", hidden_width)
     
@@ -60,26 +63,19 @@ def hidden_image(image):
                 # If Alpha is used
                 # chars.append(str(img[r,c,3] & 1))
                 count += 3
-            # # Red
-            # img[r, c][0] = 0
-            # # Green
-            # img[r, c][1] = img[r, c, 1]
-            # # Blue
-            # img[r, c][2] = 128
-            # #Alpha __
 
     chars = chars[64:]
-    output = bitarray.bitarray("".join(chars))
+    # output = bitarray("".join(chars))
     # print(output)
 
     for r in range(hidden_height):
         for c in range(hidden_width):
             # print("R:", chars[0:8], "G:", chars[8:16], "B:", chars[16:24])
 
-            red = ba2int(bitarray.bitarray("".join(chars[0:8])))
-            green = ba2int(bitarray.bitarray("".join(chars[8:16])))
-            blue = ba2int(bitarray.bitarray("".join(chars[16:24])))
-            # alpha = ba2int(bitarray.bitarray("".join(chars[24:32])))
+            red = ba2int(bitarray("".join(chars[0:8])))
+            green = ba2int(bitarray("".join(chars[8:16])))
+            blue = ba2int(bitarray("".join(chars[16:24])))
+            # alpha = ba2int(bitarray("".join(chars[24:32])))
 
             # print("R:", red, "G:", green, "B:", blue)
 
@@ -91,8 +87,24 @@ def hidden_image(image):
 
     imageio.imwrite("altered_py.png", img)
 
+def detect_hidden(image):
+    img = imageio.imread(image)
+    height, width, channels = img.shape
+    print("Height:", height, "Width:", width, "Number of Channels:", channels)
+
+    for r in range(height):
+        for c in range(width):
+            red = '00000000' if (img[r,c,0] & 1) == 0 else '11111111'
+            green = '00000000' if (img[r,c,1] & 1) == 0 else '11111111'
+            blue = '00000000' if (img[r,c,2] & 1) == 0 else '11111111'
+
+            img[r,c][0] = red
+            img[r,c][1] = green
+            img[r,c][2] = blue
+
+    imageio.imwrite("detected_py.png", img)
+
 if __name__ == "__main__":
-    # text_with_header_least_sig("sampleImages/hide_text.png")
-    hidden_image("sampleImages/hide_image.png")
-    # 2 Least significant bits
-    # image: least sig, fill rest byte
+    # text_with_header("sampleImages/hide_text.png", 3)
+    # hidden_image("sampleImages/hide_image.png")
+    detect_hidden("sampleImages/hide_image.png")
